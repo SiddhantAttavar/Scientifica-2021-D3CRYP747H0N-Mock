5c21c639553e0c9913edb27ced1874888c06bc16
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2021-09-06 12:07:05 +0530

    Move files

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import MainTemplate #type: ignore
 from anvil import *
+import Game.Game
 
 class Main(MainTemplate):
   def __init__(self, **properties):
@@ -14,5 +15,22 @@ class Main(MainTemplate):
       for j in range(self.GAME_SIZE):
         self.buttonGrid[i].append(getattr(self, f'button{i}{j}'))
         self.buttonGrid[i][j].text = ' '
+    
+    Game.displayBoard = self.displayBoard
+    self.startGame(not self.botCheckBox.checked)
+
+  def displayBoard(self):
+    # Display the cells in the grid
+    for row in range(self.GAME_SIZE):
+        for col in range(self.GAME_SIZE):
+            self.buttonGrid[row][col].configure(
+                text = str(Game.board[row][col]) if Game.board[row][col] else '',
+                bg = Game.CELL_COLOR[Game.board[row][col]]
+            )
+    
+    # Disply the updated score
+    #scoreLabel.configure(text = f'Score: {score}')
   
-  
+  def startGame(self, playerIsHuman):
+    Game.addNewNum(Game.board, False)
+    Game.addNewNum(Game.board, True)
