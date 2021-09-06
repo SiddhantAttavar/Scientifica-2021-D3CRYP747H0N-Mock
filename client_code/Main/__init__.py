f0b9d81fbb36fbf50cadb8ceb5ccf68b98c77373
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2021-09-06 11:53:13 +0530

    Fix bug and get things running

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import MainTemplate
+from ._anvil_designer import MainTemplate #type: ignore
 from anvil import *
 
 class Main(MainTemplate):
@@ -7,3 +7,12 @@ class Main(MainTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+    self.GAME_SIZE = 4
+    self.buttonGrid = []
+    for i in range(self.GAME_SIZE):
+      self.buttonGrid.append([])
+      for j in range(self.GAME_SIZE):
+        self.buttonGrid[i].append(getattr(self, f'button{i}{j}'))
+        self.buttonGrid[i][j].text = ' '
+  
+  
